54c96850d51b4e0e14d89958019887bf103a2200
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-22 04:01:57 -0800

    Refactor CharacterList component rendering

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -6,18 +6,23 @@ import {
   getCharacterProfile
 } from '../reducer/character/actions';
 
+
 // As usually one waits til its clicked to invoke the onClick, invoking onClicks in each li because the id are available during character creation mapping.
-const CharacterList = ({ characters, setCharacter }) =>
-  <div id="characeter-list" className="col-md-6">
+const CharacterList = ({ characters, setCharacter }) => (
+  <div id="characeter-list" className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <h1>Characters</h1>
+
     <ul>
       {characters.map((c, i) =>
-        <li key={c.name} onClick={setCharacter(i+ 1)}>
+        <button key={c.name}
+          onClick={setCharacter(i+ 1)}
+        >
           {c.name}
-        </li>
+        </button>
       )}
     </ul>
   </div>
+);
 
 //receive state from store which this then return what we want through component through props. (characters from store)
 const mapStateToProps = ({ characters }) => ({
