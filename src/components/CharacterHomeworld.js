7209600847fbbd20d6c0711938213a115392d975
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-22 04:01:00 -0800

    Update CharacterHomeworld component to render more results

--- src/components/CharacterHomeworld.js
+++ src/components/CharacterHomeworld.js
@@ -2,20 +2,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-
 const isKnown = text => text && text !== 'unknown';
 
 // as long as isKnown texts' exist, then the entire div will show.
-const CharacterHomeworld = ({ homeworld }) =>
-  <div id="character-profile" className="col-md-6">
-    <h1>World</h1>
-    {isKnown(homeworld.name) && <p>Name: {homeworld.name}</p>}
-    {isKnown(homeworld.population) && <p>Population: {homeworld.population}</p>}
-    {isKnown(homeworld.diatmer) && <p>Diameter: {homeworld.diatmer}km</p>}
-    {isKnown(homeworld.rotation_period) && <p>Day length: {homeworld.rotation_period} hours</p>}
-    {isKnown(homeworld.orbital_period) && <p>Year length: {homeworld.orbital_period} days</p>}
-    {isKnown(homeworld.climate) && <p>Climate: {homeworld.climate}</p>}
-  </div>;
+const CharacterHomeworld = ({ homeworld }) => (
+  <div id="character-profile" className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+    <h1>Homeworld</h1>
+
+      {isKnown(homeworld.name) && (
+        <p><b>Planet</b>: {homeworld.name}</p>
+      )}
+
+      {isKnown(homeworld.population) && (
+        <p><b>Population</b>: {homeworld.population}</p>
+      )}
+      {isKnown(homeworld.residents) && (
+        <p><b>Known Residents</b>: {homeworld.residents.length}</p>
+      )}
+
+      {isKnown(homeworld.climate) && (
+        <p><b>Climate</b>: {homeworld.climate}</p>
+      )}
+
+      {isKnown(homeworld.terrain) && (
+        <p><b>Terrain</b>: {homeworld.terrain}</p>
+      )}
+
+      {isKnown(homeworld.gravity) && (
+        <p><b>Gravity</b>: {homeworld.gravity}</p>
+      )}
+
+      {isKnown(homeworld.diatmer) && (
+        <p><b>Diameter</b>: {homeworld.diatmer}km</p>
+      )}
+
+      {isKnown(homeworld.rotation_period) && (
+        <p><b>Day length</b>: {homeworld.rotation_period} hours</p>
+      )}
+
+      {isKnown(homeworld.orbital_period) && (
+        <p><b>Year length</b>: {homeworld.orbital_period} days</p>
+      )}
+  </div>
+);
 
 const mapStateToProps = ({ character: { homeworld } }) => ({
   homeworld
