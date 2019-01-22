0069b4a34a1cae524241b2cf08b654819638e04b
Alexandra Swart <4269260+Answart@users.noreply.github.com>
2019-01-22 04:06:45 -0800

    Refactor CharacterProfile component rendering to show more results

--- src/components/CharacterProfile.js
+++ src/components/CharacterProfile.js
@@ -2,16 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-
-const CharacterProfile = ({ profile, homeworld }) =>
-  <div id="character-profile" className="col-md-6">
+const CharacterProfile = ({ profile, homeworld }) => (
+  <div id="character-profile" className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
     <h1>Profile</h1>
-    {profile.name && <p>Name: {profile.name}</p>}
-    {profile.height && <p>Height: {profile.height}cm</p>}
-    {profile.mass && <p>Weight: {profile.mass}kg</p>}
-    {homeworld.name && <p>Homeworld: {homeworld.name}</p>}
-    {profile.gender && <p>Gender: {profile.gender}</p>}
-  </div>;
+
+    {profile.name && (
+      <p><b>Name</b>: {profile.name}</p>
+    )}
+    {profile.birth_year && (
+      <p><b>Year Born</b>: {profile.birth_year}</p>
+    )}
+    {profile.height && (
+      <p><b>Height</b>: {profile.height}cm</p>
+    )}
+    {profile.mass && (
+      <p><b>Weight</b>: {profile.mass}kg</p>
+    )}
+    {profile.gender && (
+      <p><b>Gender</b>: {profile.gender}</p>
+    )}
+    {profile.hair_color && (
+      <p><b>Hair</b>: {profile.hair_color}</p>
+    )}
+    {profile.eye_color && (
+      <p><b>Eyes</b>: {profile.eye_color}</p>
+    )}
+    {profile.vehicles && (
+      <p><b>Vehicles</b>: {profile.vehicles.length}</p>
+    )}
+    {profile.starships && (
+      <p><b>Starships</b>: {profile.starships.length}</p>
+    )}
+  </div>
+)
 
 const mapStateToProps = ({ character: { profile, homeworld, movies } }) => ({
   profile,
